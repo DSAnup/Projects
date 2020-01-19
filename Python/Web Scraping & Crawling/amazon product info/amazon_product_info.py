a2d29d472bd01c06b8c5c7ce69afbfa63d1d3070
anupvhumi <anup12.m@gmail.com>
2020-01-20 02:21:04 +0600

    Amazon product info update

--- Python/Web Scraping & Crawling/amazon product info/amazon_product_info.py
+++ Python/Web Scraping & Crawling/amazon product info/amazon_product_info.py
@@ -4,38 +4,34 @@ import json
 import base64
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-print("Please enter your amazon product link")
+print("Please enter your amazon product link give a space then enter:")
 url = input().strip()
 response = requests.get(url, headers= headers)
 soup = bs4.BeautifulSoup(response.content, features='lxml')
+
 title = soup.select("#productTitle")[0].get_text().strip()
-print(title)
-categories = []
-for li in soup.select("#wayfinding-breadcrumbs_container ul.a-unordered-list")[0].findAll("li"):
-    categories.append(li.get_text().strip())
-print(categories)
-features = []
-for li in soup.select("#feature-bullets ul.a-unordered-list")[0].findAll("li"):
-    features.append(li.get_text().strip())
-print(features)
 price = soup.select("#priceblock_ourprice")[0].get_text()
-print(price)
-review_count = soup.select("#acrCustomerReviewText")[0].get_text().split()
-print(review_count)
+categories = []
+features = []
+review_count = []
+availability = []
 try:
+    for li in soup.select("#wayfinding-breadcrumbs_container ul.a-unordered-list")[0].findAll("li"):
+        categories.append(li.get_text().strip())
+    for li in soup.select("#feature-bullets ul.a-unordered-list")[0].findAll("li"):
+        features.append(li.get_text().strip())
+    review_count = soup.select("#acrCustomerReviewText")[0].get_text().split()
     availability = soup.select("#availability")[0].get_text().strip()
-    print(availability)
+
 except:
-    print('Not found')
-    availability = 'not found'
+    print('some value is missing')
+
 for li in soup.select("#imgTagWrapperId")[0].findAll("img"):
     lnk = li["src"].strip()
-    print(lnk)
     filename = title +'.jpeg'
     with open(filename, 'wb') as f:
         f.write(base64.b64decode(lnk[23:]))
-
 jsonObject = {'title': title, 'categories': categories, 'features': features, 'price': price, 'review_count': review_count, 'availability': availability}
 
-with open(title+'.json', 'w') as f:
+with open(title + '.json', 'w') as f:
     json.dump(jsonObject, f)
